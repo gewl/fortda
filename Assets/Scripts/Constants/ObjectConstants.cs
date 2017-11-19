b5ef6e095f2e75894da4c5b9db932bfc1f89c7c6
gewl <gilbertwlawson@gmail.com>
2017-11-18 19:36:58 -0500

    Fog

---
+++ Assets/Scripts/Constants/ObjectConstants.cs
@@ -0,0 +1,20 @@
+﻿public enum Colors
+{
+    Red,
+    Green,
+    Blue
+}
+
+public enum Sizes
+{
+    Small,
+    Medium,
+    Large
+}
+
+public enum Shape
+{
+    Cube,
+    Sphere,
+    Pyramid
+}
